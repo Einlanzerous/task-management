0b6d23d89af0cdf92600de94a3b2f195141d27db
einlanzerous <einlanzerous@live.com>
2020-07-01 21:53:40 -0400

    Add production params/config.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
-
-const PORT = 3001;
+import * as config from 'config';
 
 async function bootstrap() {
+  const serverConfig = config.get('server');
   const log = new Logger('bootstrap');
+  const port = process.env.PORT || serverConfig.port;
 
   const app = await NestFactory.create(AppModule);
-  await app.listen(PORT);
+  await app.listen(port);
 
-  log.log(`Application listening on port ${PORT}`);
+  log.log(`Application listening on port ${port}`);
 }
 bootstrap();
